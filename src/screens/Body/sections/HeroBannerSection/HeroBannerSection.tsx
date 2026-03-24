a348c8f35ea3c53eabc5ca93e0e9d20d36cb09cb
Sameerchoudhary8093 <Sameerchoudhary8093@gmail.com>
2026-03-24 12:29:09 +0530

    In the mobile  form will come first

--- src/screens/Body/sections/HeroBannerSection/HeroBannerSection.tsx
+++ src/screens/Body/sections/HeroBannerSection/HeroBannerSection.tsx
@@ -100,7 +100,7 @@ export const HeroBannerSection = (): JSX.Element => {
       </div>
 
       {/* Main content */}
-      <div className="relative w-full flex flex-col lg:flex-row items-center lg:items-start px-4 md:px-10 py-8 md:py-[39px] gap-8">
+      <div className="relative w-full flex flex-col-reverse lg:flex-row items-center lg:items-start px-4 md:px-10 py-8 md:py-[39px] gap-8">
         {/* Left: Hero text */}
         <div className="flex flex-col flex-1 min-w-0 pt-0 md:pt-0">
           <div className="mb-[35px]">
